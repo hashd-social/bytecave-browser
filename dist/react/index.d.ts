86a2a252f4d3234961eff3ab6ff311dc96f0a97e
Alexx Shadow <alexx@3oh.io>
2026-01-21 17:24:39 +0000

    FIX: change package scope to @hashd-social and remove dist from git

--- dist/react/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * React integration for ByteCave Browser
- *
- * Export all React hooks and components
- */
-export * from './hooks.js';
-export * from './components.js';
-export { useHashdUrl } from './useHashdUrl.js';
